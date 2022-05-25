5478723ba45df9814558b271c20fc977fc04e903
Billy <kriper01@wp.pl>
2022-05-25 23:39:03 +0200

    fixed not collapsing mobile menu when selecting option

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,21 @@
 var nav;
 var burger;
+var navOptions;
+
 document.onload = getElements();
 function getElements() {
     burger = document.getElementById("burger");
     nav = document.querySelector("ul");
+    navOptions = document.querySelector("ul").getElementsByTagName("li");
+    console.log(nav);
 }
+
+Array.from(navOptions).forEach(element => {
+    element.addEventListener("click", () => {
+        nav.classList.remove("open");
+        burger.classList.remove("crossed");
+    })
+});
 
 burger.addEventListener("click", () => {
     nav.classList.toggle("open");
