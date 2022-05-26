4ebe3a6c806898b2d29b643572218cd3e1d9666d
Billy <kriper01@wp.pl>
2022-05-26 16:50:58 +0200

    disabled scrolling on mobile menu open

--- js/nav.js
+++ js/nav.js
@@ -1,23 +1,22 @@
-var nav;
-var burger;
-var navOptions;
-
-document.onload = getElements();
-function getElements() {
-    burger = document.getElementById("burger");
-    nav = document.querySelector("ul");
-    navOptions = document.querySelector("ul").getElementsByTagName("li");
-    console.log(nav);
-}
+var burger = document.getElementById("burger");
+var nav = document.querySelector("ul");
+var navOptions = document.querySelector("ul").getElementsByTagName("li");
 
 Array.from(navOptions).forEach(element => {
     element.addEventListener("click", () => {
+        toggleHTMLOverflowY();
         nav.classList.remove("open");
         burger.classList.remove("crossed");
     })
 });
 
 burger.addEventListener("click", () => {
+    toggleHTMLOverflowY();
     nav.classList.toggle("open");
     burger.classList.toggle("crossed");
 })
+
+function toggleHTMLOverflowY(){
+    document.documentElement.style.overflowY = 
+        (document.documentElement.style.overflowY === "hidden") ? "" : "hidden";
+}
