a67f31f609f6f361b4221f1b0921186072bf8398
Billy <kriper01@wp.pl>
2022-09-21 03:43:04 +0200

    Small fixes, added makao project

--- js/slider.js
+++ js/slider.js
@@ -1,49 +1,66 @@
-
 window.addEventListener('load', OnLoading);
 var nrSlide = 0;
 
 var slides = document.getElementsByClassName("slide");
 var dots = document.getElementsByClassName("sDot");
 
-var TIMER_changeSlide = 0, fadeOut = 0;
-var sChangeDuration = 30000, fadeOutDuration = 700;
+var TIMER_changeSlide = 0,
+    fadeOut = 0;
+var sChangeDuration = 30000,
+    fadeOutDuration = 700;
 
 var slideLeft = true;
 
-function OnLoading(){
+function OnLoading() {
+    renderSlideDots();
     changeSlide();
 }
 
-function changeSlide()
-{
-    if(nrSlide > slides.length - 1) 
+function renderSlideDots() {
+    var dotsContainer = document.getElementById("buttons");
+    var dots = "";
+    for (var i = 0; i < slides.length; i++) {
+        dots += '<span class="sDot" onclick="chooseSlide(' + i + ')"></span>';
+    }
+    dotsContainer.innerHTML = dots;
+}
+
+function changeSlide() {
+    if (nrSlide > slides.length - 1)
         nrSlide = 0;
     chooseSlide(nrSlide);
 }
 
-function NextPrevButton(n) 
-{
+function NextPrevButton(n) {
     //setting flag to change direction of sliding
-    if(n == -1) {slideLeft = false}
-    else {slideLeft = true}
+    if (n == -1) {
+        slideLeft = false
+    } else {
+        slideLeft = true
+    }
 
 
     nrSlide += n - 1;
-    if(nrSlide < 0) {nrSlide = slides.length - 1;}
-    if(nrSlide > slides.length - 1) {nrSlide = 0;}
-        
+    if (nrSlide < 0) {
+        nrSlide = slides.length - 1;
+    }
+    if (nrSlide > slides.length - 1) {
+        nrSlide = 0;
+    }
+
     chooseSlide(nrSlide);
 }
 
-function chooseSlide(n)
-{
+function chooseSlide(n) {
     clearTimeout(TIMER_changeSlide);
 
-    if(n < nrSlide) {slideLeft = false};
+    if (n < nrSlide) {
+        slideLeft = false
+    };
     nrSlide = n;
     slideImage();
 
-    for(i = 0; i < slides.length; i++){
+    for (i = 0; i < slides.length; i++) {
         dots[i].style.background = "transparent";
     }
     dots[n].style.background = "red";
@@ -52,22 +69,18 @@ function chooseSlide(n)
 }
 
 
-function slideImage()
-{
+function slideImage() {
     //clearing fade out (double-click issue)
-    clearTimeout(fadeOut); 
+    clearTimeout(fadeOut);
 
     // remove active class from current activated image
-    for(i = 0; i < slides.length; i++){
+    for (i = 0; i < slides.length; i++) {
 
         /* changing sliding direction*/
-        if(slideLeft)
-        {
+        if (slideLeft) {
             slides[i].classList.add('slideLeft');
             slides[i].classList.remove('slideRight');
-        }
-        else
-        {
+        } else {
             slides[i].classList.add('slideRight');
             slides[i].classList.remove('slideLeft');
         }
@@ -75,23 +88,24 @@ function slideImage()
         // document.getElementById("bel").src = "attachments/images.jpg";
         slides[i].classList.remove('active');
         slides[i].style.position = "absolute";
-        
+
     }
     //displaying slide
-    if(nrSlide == slides.length){ nrSlide = 0; }
+    if (nrSlide == slides.length) {
+        nrSlide = 0;
+    }
     slides[nrSlide].style.display = 'block';
     slides[nrSlide].style.position = 'relative';
-            
+
     slides[nrSlide].classList.add('active');
-        nrSlide++;
+    nrSlide++;
 
     // wait for fade effect to finish,  then add display none to current image and activate next one
     fadeOut = setTimeout(
-        function()
-        {                 
+        function () {
             //add display none to current image
-            for(i = 0; i < slides.length; i++){
-                if(nrSlide - 1 != i){
+            for (i = 0; i < slides.length; i++) {
+                if (nrSlide - 1 != i) {
                     slides[i].style.display = 'none';
                 }
                 //reseting sliding direction
@@ -99,4 +113,3 @@ function slideImage()
             }
         }, fadeOutDuration);
 }
-
